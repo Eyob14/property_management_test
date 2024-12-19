d471252499ed84850ae0a9d7f34c7df4e01ede60
Eyob14 <eyobzebene3580@gmail.com>
2024-12-19 17:01:31 +0300

    relocate models to services

--- property_management_service/src/app.module.ts
+++ property_management_service/src/app.module.ts
@@ -3,19 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './modules/auth/auth.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { PropertiesModule } from './modules/properties/properties.module';
-import { JobsModule } from './modules/jobs/jobs.module';
 import { TenanciesModule } from './modules/tenancies/tenancies.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_DB_CONN_URI),
-    AuthModule,
     PropertiesModule,
-    JobsModule,
     TenanciesModule,
     LoggerModule,
   ],
